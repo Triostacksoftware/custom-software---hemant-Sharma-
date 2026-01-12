98f77d1be1135182ffa9c85527ffaf309a60cda4
gitBySharma <fetchsubhankar@gmail.com>
2026-01-12 15:59:51 +0530

    first commit

---
+++ server/src/app.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+mongoose.connect(process.env.DATABASE_CONNECTION_STRING)
+    .then(() => {
+        console.log("Database connection established");
+        app.listen(process.env.PORT, () => {
+            console.log(`Server running on port ${process.env.PORT}`);
+        })
+    })
+    .catch((error) => {
+        console.log("Database connection error", error);
+    })
