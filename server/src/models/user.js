53d42abc2e2202de1229ba7e6650367960304c09
gitBySharma <fetchsubhankar@gmail.com>
2026-01-14 13:57:37 +0530

    user schema added

---
+++ server/src/models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+const PHONE_REGEX = /^\+?[1-9]\d{1,14}$/;
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, "Name is required"],
+    },
+    phoneNumber: {
+        type: String,
+        required: [true, "Phone number is required"],
+        trim: true,
+        unique: true,
+        validate: {
+            validator: v => PHONE_REGEX.test(v),   // This regex validates E.164 format (e.g. +1234567890)
+            message: ({ value }) => `${value} is not a valid phone number`
+        }
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"]
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model("User", userSchema)
+
